f2faa8cbc95620c17bb4fdfbe2b09f711e4d7682
unknown <petrov_desislav@abv.bg>
2019-07-29 01:22:11 +0300

    project commit

---
+++ script.js
@@ -0,0 +1,60 @@
+// Base tile layers
+var esriWorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+}),
+esriWorldImageryMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+  osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }),
+  wikimedia = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
+    attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>'
+  });
+
+var map = L.map('map', { layers: [esriWorldStreetMap] }).setView([42.664602, 23.317155], 4);
+
+var baseLayers = {
+  "ESRI Street": esriWorldStreetMap,
+  "ESRI Imagery": esriWorldImageryMap,
+  "Open Street Map": osm,
+  "Wikimedia": wikimedia
+};
+
+L.control.layers(baseLayers).addTo(map);
+
+// Reverse geocoding
+var geocodeService = L.esri.Geocoding.geocodeService();
+
+var reverseGeocodeLayerGroup = L.layerGroup().addTo(map);
+
+map.on('click', function (e) {
+  geocodeService.reverse().latlng(e.latlng).run(function (error, result) {
+    reverseGeocodeLayerGroup.addLayer(L.marker(result.latlng).addTo(map).bindPopup(result.address.Match_addr).openPopup());
+  });
+});
+
+// Button for deleting reverse geocoding markers
+var controlEasyButton = L.easyButton({
+  states: [{
+    icon: 'glyphicon-remove',
+    title: 'Remove Markers',
+    onClick: function () {
+      // reverseGeocodeLayerGroup.clearLayers();
+      map.removeLayer(reverseGeocodeLayerGroup);
+    }
+  }]
+}).addTo(map);
+
+// Geocoding
+var searchControl = L.esri.Geocoding.geosearch().addTo(map);
+
+var results = L.layerGroup().addTo(map);
+
+searchControl.on('results', function (data) {
+  results.clearLayers();
+  for (var i = data.results.length - 1; i >= 0; i--) {
+    results.addLayer(L.marker(data.results[i].latlng));
+  }
+});
+
